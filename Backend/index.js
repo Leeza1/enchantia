1e840be48e2dbf993483020f24e04b2eac1a6db9
Rajila <rajila@example.com>
2023-06-02 08:49:19 +0545

    backend changes

--- Backend/index.js
+++ Backend/index.js
@@ -1,50 +1,24 @@
 const express = require('express');
 const connectDB = require('./database/Database');
-const cors = require('cors');
-const cloudinary = require('cloudinary');
-const multipart = require('connect-multiparty');
 
-// Dotenv Config
-require("dotenv").config();
 const app = express();
 
-// express json
+//dotenv config
+require("dotenv").config();
+
+//middleware express json
 app.use(express.json());
-app.use(multipart())
 
-// cors config
-const corsOptions = {
-    origin:true,
-    credentials: true,
-    optionSuccessStatus: 200
-};
-// Configuration 
-cloudinary.config({
-    cloud_name: "dk08ni3q5",
-    api_key: "249642849958887",
-    api_secret: "V4UEvb55JSNBY_Uel5rQ7a1JfyI"
-  });
-
-app.use(cors(corsOptions));
-
-//  create a route
-app.get('/', (req, res) => {
-    res.send('Welcome to API');
+//Defining Routes
+app.get('/', (req, res)=>{
+    res.send('Hello World!');
 });
 
-// middleware for user controller
-app.use('/api/user', require('./controllers/userControllers'));
-//middleware for product controllers
-app.use('/api/product', require('./controllers/productControllers')); 
-
-
-// connect to database
+//connect to databasse
 connectDB();
 
-// listen to the server
+//listen to the server
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
 });
-
-
 
